485ff122ae4462565657831a83f9cc56c3e55647
Daniel Tse <danieltse@gmail.com>
2017-05-02 00:18:55 +1000

    first commit

---
+++ src/components/Summary.js
@@ -0,0 +1,27 @@
+const Summary = createClass({
+  displayName: "Summary",
+  propTypes: {
+    ingredients: PropTypes.array.isRequired,
+    steps: PropTypes.array.isRequired,
+    title: PropTypes.string
+  },
+  getDefaultProps() {
+      return {
+          ingredients: 0,
+          steps: 0,
+          title: "[recipe]"
+      }
+  },
+  render() {
+    const {ingredients, steps, title} = this.props
+    return (
+      <div className="summary">
+        <h1>{title}</h1>
+        <p>
+          <span>{ingredients.length} Ingredients</span> |
+          <span>{steps.length} Steps</span>
+        </p>
+      </div>
+    )
+  }
+})
